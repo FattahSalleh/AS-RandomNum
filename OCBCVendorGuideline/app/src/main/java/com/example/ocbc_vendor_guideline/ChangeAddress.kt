61419832653146db318589a003e492f21dec7f7f
FattahSalleh <fattah.sallehuddin@gmail.com>
2021-04-12 19:36:05 +0800

    Init 2nd page: Change Address

--- OCBCVendorGuideline/app/src/main/java/com/example/ocbc_vendor_guideline/ChangeAddress.kt
+++ OCBCVendorGuideline/app/src/main/java/com/example/ocbc_vendor_guideline/ChangeAddress.kt
@@ -2,6 +2,7 @@ package com.example.ocbc_vendor_guideline
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Html
 
 class ChangeAddress : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,8 +10,11 @@ class ChangeAddress : AppCompatActivity() {
         setContentView(R.layout.activity_change_address)
 
         val actionbar = supportActionBar
-        actionbar!!.title = "Change Address"
+//        actionbar!!.title = "Change Address"
+        actionbar!!.title = Html.fromHtml("<font color=\"black\">Change Address</font>")
         actionbar.setDisplayHomeAsUpEnabled(true)
+
+        TODO("Change color of arrow for back button to BLACK")
     }
 
     override fun onSupportNavigateUp(): Boolean {
